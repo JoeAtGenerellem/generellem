291d1ad8251f3eae7caea15d8f83a56eab5cf9e2
Joe Mayo <jmayo@mayosoftware.com>
2023-12-01 22:08:02 -0800

    More unit tests.
    #31

---
+++ Generellem.Tests/AzureOpenAIRagTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Security;
+
+using Generellem.Rag;
+using Generellem.Rag.AzureOpenAI;
+using Generellem.Security;
+using Generellem.Services.Azure;
+
+using Moq;
+
+namespace Generellem.Tests;
+public class AzureOpenAIRagTests
+{
+    Mock<ISecretStore> secretStoreMock = new();
+    Mock<IAzureSearchService> searchSvc = new();
+
+    IRag azureOpenAIRag;
+
+    public AzureOpenAIRagTests()
+    {
+        azureOpenAIRag = new AzureOpenAIRag(secretStoreMock.Object, searchSvc.Object);
+    }
+
+    [Fact]
+    public async Task IndexAsync_CallsCreateIndex()
+    {
+        var chunks = new List<TextChunk>();
+
+        await azureOpenAIRag.IndexAsync(chunks, CancellationToken.None);
+
+        searchSvc.Verify(x => x.CreateIndexAsync(), Times.Once());
+    }
+
+    [Fact]
+    public async Task IndexAsync_CallsUploadDocuments()
+    {
+        var chunks = new List<TextChunk>();
+
+        await azureOpenAIRag.IndexAsync(chunks, CancellationToken.None);
+
+        searchSvc.Verify(x => x.UploadDocumentsAsync(chunks), Times.Once());
+    }
+
+    [Fact]
+    public async Task IndexAsync_CallsUploadDocumentsWithCorrectChunks()
+    {
+        var chunks = new List<TextChunk> { /* populate */ };
+
+        await azureOpenAIRag.IndexAsync(chunks, CancellationToken.None);
+
+        searchSvc.Verify(x => x.UploadDocumentsAsync(It.Is<List<TextChunk>>(c => c == chunks)), Times.Once());
+    }
+}
