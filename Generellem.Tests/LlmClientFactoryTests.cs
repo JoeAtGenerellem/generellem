96bff4bc1dcec747980c987e68e1af4b67c52f3f
Joe Mayo <jmayo@mayosoftware.com>
2023-12-09 17:02:36 -0800

    LLM Unit Tests
    Added unit tests for AzureOpenAILlm and LlmClientFactory.
    
    #27

---
+++ Generellem.Tests/LlmClientFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using Azure.AI.OpenAI;
+
+using Generellem.Llm;
+using Generellem.Services;
+
+using Microsoft.Extensions.Configuration;
+
+using Moq;
+
+namespace Generellem.Tests;
+
+public class LlmClientFactoryTests
+{
+    Mock<IConfiguration> configMock = new();
+
+    LlmClientFactory factory;
+
+    public LlmClientFactoryTests()
+    {
+        configMock
+            .Setup(config => config[GKeys.AzOpenAIEndpointName])
+            .Returns("https://generellem");
+        configMock
+            .Setup(config => config[GKeys.AzOpenAIApiKey])
+            .Returns("generellem-key");
+
+        factory = new(configMock.Object);
+    }
+
+    [Fact]
+    public void CreateOpenAIClient_ReturnsOpenAIClient()
+    {
+        var result = factory.CreateOpenAIClient();
+
+        Assert.IsType<OpenAIClient>(result);
+    }
+}
---
+++ Generellem.Tests/TestEmbeddings.cs
@@ -0,0 +1,8 @@
+﻿namespace Generellem.Tests;
+
+internal class TestEmbeddings
+{
+    const int EmbeddingVectorSize = 1536;
+
+    public static float[] CreateEmbeddingArray() => Enumerable.Range(1, EmbeddingVectorSize).Select(i => i * 1f).ToArray();
+}
