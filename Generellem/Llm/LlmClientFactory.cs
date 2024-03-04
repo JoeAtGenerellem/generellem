35c61856724c77b1e8d42ee4389e8a52582d28f5
Joe Mayo <6180567+JoeMayo@users.noreply.github.com>
2024-03-03 22:40:51 -0800

    Added IGenerellemConfiguration (#126)
    Changed configuration to a new type, IGenerellemConfiguration. This will let us add dynamic configuration for a user. Normal configuration is still valid because the change makes us check dynamic configuration first and then use normal configuration if it doesn't find the value.
    
    Closes #125

--- Generellem/Llm/LlmClientFactory.cs
+++ Generellem/Llm/LlmClientFactory.cs
@@ -3,13 +3,11 @@ using Azure.AI.OpenAI;
 
 using Generellem.Services;
 
-using Microsoft.Extensions.Configuration;
-
 namespace Generellem.Llm;
 
 public class LlmClientFactory
 {
-    readonly IConfiguration config;
+    readonly IGenerellemConfiguration config;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     /// <summary>
@@ -18,7 +16,7 @@ public class LlmClientFactory
     protected LlmClientFactory() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-    public LlmClientFactory(IConfiguration config)
+    public LlmClientFactory(IGenerellemConfiguration config)
     {
         this.config = config;
     }
