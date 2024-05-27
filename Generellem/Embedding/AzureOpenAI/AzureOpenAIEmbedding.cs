f71191c0e219573ad150c20e409411f51c6cbb89
Joe Mayo <jmayo@mayosoftware.com>
2024-05-27 16:13:53 -0700

    Made progress reporting more granular, by including chunks.

--- Generellem/Embedding/AzureOpenAI/AzureOpenAIEmbedding.cs
+++ Generellem/Embedding/AzureOpenAI/AzureOpenAIEmbedding.cs
@@ -3,6 +3,7 @@ using Azure.AI.OpenAI;
 
 using Generellem.Document.DocumentTypes;
 using Generellem.Llm;
+using Generellem.Processors;
 using Generellem.Rag;
 using Generellem.Services;
 using Generellem.Services.Exceptions;
@@ -19,16 +20,28 @@ public class AzureOpenAIEmbedding(
     ILogger<AzureOpenAIEmbedding> logger)
     : IEmbedding
 {
+    static IProgress<IngestionProgress>? currentProgress = null;
+
     readonly OpenAIClient openAIClient = llmClientFact.CreateOpenAIClient();
 
     readonly ResiliencePipeline pipeline =
-    new ResiliencePipelineBuilder()
-        .AddRetry(new()
-        {
-            ShouldHandle = new PredicateBuilder().Handle<Exception>(ex => ex is not GenerellemNeedsIngestionException)
-        })
-        .AddTimeout(TimeSpan.FromSeconds(7))
-        .Build();
+        new ResiliencePipelineBuilder()
+            .AddRetry(new()
+            {
+                ShouldHandle = new PredicateBuilder().Handle<Exception>(ex => ex is not GenerellemNeedsIngestionException)
+            })
+            .AddRetry(new()
+            {
+                BackoffType = DelayBackoffType.Exponential,
+                Delay = TimeSpan.FromSeconds(5),
+                MaxRetryAttempts = 5,
+                OnRetry = static args =>
+                {
+                    currentProgress?.Report(new("System busy, delaying a few seconds..."));
+                    return default;
+                }
+            })
+            .Build();
 
     /// <summary>
     /// Breaks text into chunks and adds an embedding to each chunk based on the text in that chunk.
@@ -36,22 +49,31 @@ public class AzureOpenAIEmbedding(
     /// <param name="fullText">Full document text.</param>
     /// <param name="docType"><see cref="IDocumentType"/> for extracting text from document.</param>
     /// <param name="documentReference">Reference to file. e.g. either a path, url, or some other indicator of where the file came from.</param>
+    /// <param name="progress">Reports progress.</param>
     /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
     /// <returns>List of <see cref="TextChunk"/></returns>
-    public virtual async Task<List<TextChunk>> EmbedAsync(string fullText, IDocumentType docType, string documentReference, CancellationToken cancellationToken)
+    public virtual async Task<List<TextChunk>> EmbedAsync(string fullText, IDocumentType docType, string documentReference, IProgress<IngestionProgress> progress, CancellationToken cancellationToken)
     {
+        currentProgress = progress;
+
         List<TextChunk> chunks = TextProcessor.BreakIntoChunks(fullText, documentReference);
+
+        int chunkCount = chunks.Count;
+        int count = 0;
+        string plural = chunkCount == 1 ? "" : "s";
 
         foreach (TextChunk chunk in chunks)
         {
             if (chunk.Content is null) continue;
+
+            progress.Report(new($"Processing {++count} of {chunkCount} chunk{plural}"));
 
             try
             {
                 EmbeddingsOptions embeddingsOptions = GetEmbeddingOptions(chunk.Content);
                 Response<Embeddings> embeddings = await pipeline.ExecuteAsync<Response<Embeddings>>(
                     async token => await openAIClient.GetEmbeddingsAsync(embeddingsOptions, token),
-                cancellationToken);
+                    cancellationToken);
 
                 chunk.Embedding = embeddings.Value.Data[0].Embedding;
             }
--- Generellem/Embedding/IEmbedding.cs
+++ Generellem/Embedding/IEmbedding.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.AI.OpenAI;
 
 using Generellem.Document.DocumentTypes;
+using Generellem.Processors;
 
 namespace Generellem.Embedding;
 
@@ -12,9 +13,10 @@ public interface IEmbedding
     /// <param name="fullText">Document to embed.</param>
     /// <param name="docType">An <see cref="IDocumentType"/> for turning the document into text.</param>
     /// <param name="fileName">Name of file in embedding.</param>
+    /// <param name="progress">Reports progress.</param>
     /// <param name="cancellationToken"><see cref="CancellationToken"/></param>
     /// <returns><see cref="List{T}"/> of <see cref="TextChunk"/>, which holds both the real and embedded content.</returns>
-    Task<List<TextChunk>> EmbedAsync(string fullText, IDocumentType docType, string fileName, CancellationToken cancellationToken);
+    Task<List<TextChunk>> EmbedAsync(string fullText, IDocumentType docType, string fileName, IProgress<IngestionProgress> progress, CancellationToken cancellationToken);
 
     /// <summary>
     /// Embedding Options for Azure Search.
