49d99204b6697193aefcad84072b9f0a0baf750b
Joe Mayo <6180567+JoeMayo@users.noreply.github.com>
2024-10-12 22:44:49 -0700

    BaseURL (#177)
    * Made DocumentHashRepository async.
    
    * Fixed document hash logic on inserts.
    
    * Added base URL so that MSGraph implementations can build a returnURL.

--- Generellem/DocumentSource/IMSGraphClientFactory.cs
+++ Generellem/DocumentSource/IMSGraphClientFactory.cs
@@ -10,6 +10,8 @@ public interface IMSGraphClientFactory
     /// <summary>
     /// Instantiates a new <see cref="GraphServiceClient"/> for accessing MSGraph.
     /// </summary>
+    /// <param name="scopes">The scopes to request.</param>
+    /// <param name="baseUrl">The base URL to build a return URL for the OAuth flow.</param>
     /// <returns><see cref="GraphServiceClient"/></returns>
-    Task<GraphServiceClient> CreateAsync(string scopes);
+    Task<GraphServiceClient> CreateAsync(string scopes, string baseUrl);
 }
